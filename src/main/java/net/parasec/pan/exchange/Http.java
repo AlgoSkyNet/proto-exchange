dabf785bde81ae55d6ecc67cf5cd8d1b64d62d1a
Phil Stubbings <phil@parasec.net>
2017-12-03 02:44:27 +0000

    10 - 30ms speedup

--- src/main/java/net/parasec/pan/exchange/Http.java
+++ src/main/java/net/parasec/pan/exchange/Http.java
@@ -9,13 +9,23 @@ import org.apache.http.client.entity.*;
 
 import java.util.*;
 
+import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+
 
 // http://hc.apache.org/httpcomponents-client-ga/tutorial/html/fundamentals.html#d5e37
 
 public class Http {
 
-	private final HttpClient httpclient = HttpClients.createDefault();
+	private final HttpClient httpclient;
 
+
+	public Http() {
+		this.httpclient = new DefaultHttpClient(); //HttpClients.createDefault();
+		// The cost of this operation is about 15-30 ms, depending on the JRE used. Disabling stale connection check may result in slight performance improvement, especially for small payload responses, at the risk of getting an I/O error when executing a request over a connection that has been closed at the server side
+		// http://hc.apache.org/httpclient-3.x/performance.html
+		this.httpclient.getParams().setParameter("http.connection.stalecheck", false);
+	}
 
   	public String post(String url, Map<String,String> paramMap) {
     		HttpPost httppost = new HttpPost(url);
@@ -67,8 +77,8 @@ public class Http {
 				return null;
 
       			java.io.InputStream is = entity.getContent();
-
-      			try {
+ 
+     			try {
           			return convertStreamToString(is);
       			} finally {
         			is.close();
@@ -81,8 +91,16 @@ public class Http {
     		return null;
   	}
 
-    	private String convertStreamToString(java.io.InputStream is) {
-		final java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
-		return s.hasNext() ? s.next() : "";
+    	private String convertStreamToString(final InputStream is) throws Exception {
+		//final java.util.Scanner s = new java.util.Scanner(is, "UTF-8").useDelimiter("\\A");
+		//return s.hasNext() ? s.next() : "";
+		// https://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+		final ByteArrayOutputStream result = new ByteArrayOutputStream();
+		final byte[] buffer = new byte[1024];
+		int len;
+		while ((len = is.read(buffer)) != -1) {
+    			result.write(buffer, 0, len);
+		}
+		return result.toString("UTF-8");
     	}
 }
